00b5eed42e75834737111c57297ce74c513ba89a
Vitor Dino <vitor@kunstdesign.com.br>
2017-12-06 06:19:56 -0200

    content: 01

---
+++ public/course/01-intro/05-exercicios/1003.py
@@ -0,0 +1,8 @@
+# URI 1003
+
+A = int(input())            # seta a variável A de acordo com o primeiro input
+B = int(input())            # seta a variável B de acordo com o segundo input
+
+soma = A+B                  # seta a variável soma para A+B
+
+print("SOMA = %d" %soma)    # imprime a concatenação de "SOMA = " + variável soma
